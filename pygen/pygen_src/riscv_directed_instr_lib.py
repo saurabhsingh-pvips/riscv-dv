a49d5511b4117a4667098314f2d0527bcf05c416
pvipsyash <yash.p@perfectvips.com>
2020-07-06 10:01:24 +0530

    added constraint part in riscv_int_numeric_corner_stream

--- pygen/pygen_src/riscv_directed_instr_lib.py
+++ pygen/pygen_src/riscv_directed_instr_lib.py
@@ -13,7 +13,11 @@ See the License for the specific language governing permissions and
 limitations under the License.
 Regression script for RISC-V random instruction generator
 """
-from riscv_instr_stream import riscv_rand_instr_stream
+from pygen_src.riscv_instr_stream import riscv_rand_instr_stream
+from pygen_src.isa.riscv_instr import riscv_instr_ins
+from enum import Enum, auto
+import vsc
+
 
 class riscv_directed_instr_stream(riscv_rand_instr_stream):
 
@@ -32,56 +36,66 @@ class riscv_directed_instr_stream(riscv_rand_instr_stream):
             self.instr_list[0].label = self.label
             self.instr_list[0].has_label = 1
 
+
+class int_numeric_e(Enum):
+    NormalValue = auto()
+    Zero = auto()
+    AllOne = auto()
+    NegativeMax = auto()
+
+
+@vsc.randobj
 class riscv_int_numeric_corner_stream(riscv_directed_instr_stream):
 
     def __init__(self):
         riscv_directed_instr_stream.__init__(self)
-        self.num_of_avail_regs = 10
-        self.num_of_instr = 0
-        #self.int_numeric_e = ['NormalValue', 'Zero', 'AllOne', 'NegativeMax']
-        self.init_val_type = ['NormalValue', 'Zero', 'AllOne', 'NegativeMax']
+        self.num_of_avail_regs = vsc.uint8_t(10)
+        self.num_of_instr = vsc.rand_uint8_t()
+        self.init_val = vsc.list_t(vsc.rand_bit_t())
         self.init_instr = []
-    '''
-    #TO DO
-    constraint init_val_c {
-        solve init_val_type before init_val;
-        init_val_type.size() == num_of_avail_regs;
-        init_val.size() == num_of_avail_regs;
-        num_of_instr inside {[15:30]};
-    }
+        self.int_numeric_v = vsc.rand_enum_t(int_numeric_e)
+        self.init_val_type = []
 
-    #TO DO
-    constraint avail_regs_c {
-        unique {avail_regs};
-        foreach(avail_regs[i]) {
-        !(avail_regs[i] inside {cfg.reserved_regs});
-        avail_regs[i] != ZERO;
-        }
-    }
-    '''
+    @vsc.constraint
+    def init_val_c(self):
+        # TO DO
+        # solve init_val_type before init_val;
+        # len(self.init_val_type) == self.num_of_avail_regs
+        # len(self.init_val) == self.num_of_avail_regs
+        self.num_of_instr in vsc.rangelist(vsc.rng(15, 30))
+
+    @vsc.constraint
+    def avail_regs_c(self):
+        vsc.unique(self.avail_regs)
+        for i in len(self.avail_regs):
+            # self.avail_regs[i] not in cfg.reserved_regs
+            self.avail_regs[i] != 0
+
     def pre_randomize(self):
         self.avail_regs = [0] * self.num_of_avail_regs
         super().pre_randomize()
 
     def post_randomize(self):
-        self.init_instr = [0] *  self.num_of_avail_regs
-        for i in self.init_val_type:
-            if self.init_val_type[i] == 'Zero':
+        self.init_instr = [0] * self.num_of_avail_regs
+	# TODO
+        """
+        for i in range(len(self.init_val_type)):
+            if self.init_val_type[i] == Zero:
                 self.init_val[i] = 0
-            elif self.init_val_type[i] == 'AllOne':
+            elif self.init_val_type[i] == AllOne:
                 self.init_val[i] = 1
-            elif self.init_val_type[i] == 'NegativeMax':
-                self.init_val[i] = 1 << (riscv_instr_ins.XLEN-1)
+            elif self.init_val_type[i] == NegativeMax:
+                self.init_val[i] = 1 << (riscv_instr_ins.XLEN - 1)
 
             self.init_instr[i].rd = self.avail_regs[i]
-            self.init_instr[i].pseudo_instr_name = LI
+            self.init_instr[i].pseudo_instr_name = 'LI'
             self.init_instr[i].imm_str = "0x%0x" % (self.init_val[i])
-            self.init_list.append(self.init_instr[i])
-        
-        for i in range(0, self.num_of_instr): 
-            instr = riscv_instr_ins.get_rand_instr(\
-                include_category = ['ARITHMETIC'], \
-                    exclude_group = ['RV32C', 'RV64C', 'RV32F', 'RV64F', 'RV32D', 'RV64D'])
-            instr = super().randomize_gpr(instr) 
+            self.instr_list.append(self.init_instr[i])
+        """
+        for i in range(0, self.num_of_instr):
+            instr = riscv_instr_ins.get_rand_instr(
+                include_category = ['ARITHMETIC'],
+                exclude_group = ['RV32C', 'RV64C', 'RV32F', 'RV64F', 'RV32D', 'RV64D'])
+            instr = super().randomize_gpr(instr)
             self.instr_list.append(instr)
         super().post_randomize()
