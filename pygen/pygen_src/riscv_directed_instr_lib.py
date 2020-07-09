d6313c96b6d82271ea1e976c8edfc212598a687f
pvipsyash <yash.p@perfectvips.com>
2020-07-09 17:55:24 +0530

    added all constraints other than 'solve before' in riscv_int_numeric_corner_stream class

--- pygen/pygen_src/riscv_directed_instr_lib.py
+++ pygen/pygen_src/riscv_directed_instr_lib.py
@@ -14,7 +14,7 @@ limitations under the License.
 Regression script for RISC-V random instruction generator
 """
 from pygen_src.riscv_instr_stream import riscv_rand_instr_stream
-from pygen_src.isa.riscv_instr import riscv_instr_ins
+from pygen_src.isa.riscv_instr import riscv_instr_ins, cfg
 from enum import Enum, auto
 import vsc
 
@@ -51,24 +51,23 @@ class riscv_int_numeric_corner_stream(riscv_directed_instr_stream):
         riscv_directed_instr_stream.__init__(self)
         self.num_of_avail_regs = vsc.uint8_t(10)
         self.num_of_instr = vsc.rand_uint8_t()
-        self.init_val = vsc.list_t(vsc.rand_bit_t())
+        self.init_val = vsc.randsz_list_t(vsc.rand_bit_t())
+        self.init_val_type = vsc.randsz_list_t(vsc.enum_t(int_numeric_e))
         self.init_instr = []
-        self.int_numeric_v = vsc.rand_enum_t(int_numeric_e)
-        self.init_val_type = []
 
     @vsc.constraint
     def init_val_c(self):
         # TO DO
         # solve init_val_type before init_val;
-        # len(self.init_val_type) == self.num_of_avail_regs
-        # len(self.init_val) == self.num_of_avail_regs
+        self.init_val_type.size in vsc.rangelist(self.num_of_avail_regs)
+        self.init_val.size in vsc.rangelist(self.num_of_avail_regs)
         self.num_of_instr in vsc.rangelist(vsc.rng(15, 30))
 
     @vsc.constraint
     def avail_regs_c(self):
         vsc.unique(self.avail_regs)
-        for i in len(self.avail_regs):
-            # self.avail_regs[i] not in cfg.reserved_regs
+        with vsc.foreach(self.avail_regs, idx = True) as i:
+            self.avail_regs[i].not_inside(cfg.reserved_regs)
             self.avail_regs[i] != 0
 
     def pre_randomize(self):
@@ -77,21 +76,18 @@ class riscv_int_numeric_corner_stream(riscv_directed_instr_stream):
 
     def post_randomize(self):
         self.init_instr = [0] * self.num_of_avail_regs
-	# TODO
-        """
         for i in range(len(self.init_val_type)):
-            if self.init_val_type[i] == Zero:
+            if self.init_val_type[i] == int_numeric_e.Zero:
                 self.init_val[i] = 0
-            elif self.init_val_type[i] == AllOne:
+            elif self.init_val_type[i] == int_numeric_e.AllOne:
                 self.init_val[i] = 1
-            elif self.init_val_type[i] == NegativeMax:
+            elif self.init_val_type[i] == int_numeric_e.NegativeMax:
                 self.init_val[i] = 1 << (riscv_instr_ins.XLEN - 1)
 
             self.init_instr[i].rd = self.avail_regs[i]
             self.init_instr[i].pseudo_instr_name = 'LI'
             self.init_instr[i].imm_str = "0x%0x" % (self.init_val[i])
             self.instr_list.append(self.init_instr[i])
-        """
         for i in range(0, self.num_of_instr):
             instr = riscv_instr_ins.get_rand_instr(
                 include_category = ['ARITHMETIC'],
